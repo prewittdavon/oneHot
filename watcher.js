3d80a31fdf43e12b55c24aef44e44bf0d3a9a186
prewittdavon <davonprewitt@lawn-128-61-63-241.lawn.gatech.edu>
2018-02-08 15:05:12 -0500

    Added ability to remove file from folder once added to DB

--- watcher.js
+++ watcher.js
@@ -1,5 +1,6 @@
 var watcher = require('chokidar');
 var csv = require("fast-csv");
+var fs = require("fs");
  
 
 // One-liner for current directory, ignores .dotfiles
@@ -9,12 +10,18 @@ watcher.watch('.', {ignored: /(^|[\/\\])\../}).on('add', (path) => {
 	if (path.indexOf(".csv") != -1) {
 		csv.fromPath(path)
 		 .on("data", function(data){
-		     console.log(data);
+		     //console.log(data);
 		 })
 		 .on("end", function(){
-		     console.log("done parsing");
+		 	fs.unlink("./"+ path, (err) => {
+	        if (err) {
+	            console.log('Error: Failed to delete ' + path + ' ' + err);
+	        } else {
+	            console.log('successfully deleted ' + path);                                
+	        }
+		 });
+		 console.log("successfully added " + path);
 		 });
 	}
-			//3. Add to SQLDB
-  //console.log(path);
+	//3. Add to SQLDB
 });
