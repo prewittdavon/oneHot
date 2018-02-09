c3980cea088cee46e3fd01a4780178877a926797
prewittdavon <davonprewitt@lawn-128-61-63-241.lawn.gatech.edu>
2018-02-08 23:33:43 -0500

    Added ability to write rows to database

--- watcher.js
+++ watcher.js
@@ -1,27 +1,62 @@
+/*
+* Interactive folder that allows you to add CSVs into different tables
+* within a database by simply copying it over.
+* 
+* @author Davon Prewitt
+* @version 1.0.0
+* 
+*/
 var watcher = require('chokidar');
+
 var csv = require("fast-csv");
 var fs = require("fs");
- 
 
-// One-liner for current directory, ignores .dotfiles
-var csvs = {csvs: []};
+const sqlite3 = require('sqlite3')
+	.verbose();
+var db = new sqlite3.Database('./db/data.db', (err) => {
+	if (err) {
+		return console.error(err.message);
+	}
+	console.log('Connected to DB.');
+});
 
+
+var index = 0;
 watcher.watch('.', {ignored: /(^|[\/\\])\../}).on('add', (path) => {
 	if (path.indexOf(".csv") != -1) {
 		csv.fromPath(path)
 		 .on("data", function(data){
-		     //console.log(data);
+		 	//DATABSE
+		 	if (index == 0) {
+		 		columns = '(';
+		 		columnsInsert = '(';
+		 		data.forEach(function(item, index, array) {
+				  columns = columns + item + ' ' + 'text, ';
+				  columnsInsert = columnsInsert + item +', ';
+				})
+				columns = columns.substr(0,columns.length - 2) + ')';
+				columnsInsert = columnsInsert.substr(0,columnsInsert.length - 2) + ')';
+
+		 		tableName = path.substr(0, path.indexOf('.csv'));
+				db.run('CREATE TABLE ' + 'test' + ' '  + columns);
+		 	} else {
+		 		db.run('INSERT INTO ' + 'test' + ' ' + columnsInsert + ' VALUES(?,?,?,?,?,?,?)',data, function(err){
+		 			if (err) {
+		 				console.log(err);
+		 			} 
+				});
+		 	}
+		 	index++;
 		 })
 		 .on("end", function(){
 		 	fs.unlink("./"+ path, (err) => {
-	        if (err) {
-	            console.log('Error: Failed to delete ' + path + ' ' + err);
-	        } else {
-	            console.log('successfully deleted ' + path);                                
-	        }
-		 });
-		 console.log("successfully added " + path);
+		        if (err) {
+		            console.log('Error: Failed to delete ' + path + ' ' + err);
+		        } else {
+		            console.log('Successfully deleted ' + path);                                
+		        }
+		 	});
+		 console.log("Successfully added " + index + " items from " + path.substr(0, path.indexOf('.csv')));
 		 });
 	}
-	//3. Add to SQLDB
 });
